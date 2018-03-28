6596402b3602681939351d829ea113e4b1e8607d
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-03-28 10:25:44 +0200

    Fixed: Drop doesn't always work in Edge

--- plugins/clipboard/plugin.js
+++ plugins/clipboard/plugin.js
@@ -1469,14 +1469,8 @@
 			// we drop image it will overwrite document.
 
 			editable.attachListener( dropTarget, 'dragover', function( evt ) {
-				// Edge requires this handler to have `preventDefault()` regardless of the situation.
-				if ( CKEDITOR.env.edge ) {
-					evt.data.preventDefault();
-					return;
-				}
 
 				var target = evt.data.getTarget();
-
 				// Prevent reloading page when dragging image on empty document (https://dev.ckeditor.com/ticket/12619).
 				if ( target && target.is && target.is( 'html' ) ) {
 					evt.data.preventDefault();
@@ -1488,7 +1482,7 @@
 				// if we prevent it the cursor will not we shown, so we prevent
 				// dragover only on IE, on versions which support file API and only
 				// if the event contains files.
-				if ( CKEDITOR.env.ie &&
+				if ( CKEDITOR.env.ie && !CKEDITOR.env.edge &&
 					CKEDITOR.plugins.clipboard.isFileApiSupported &&
 					evt.data.$.dataTransfer.types.contains( 'Files' ) ) {
 					evt.data.preventDefault();
