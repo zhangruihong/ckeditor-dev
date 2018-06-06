1f1a7e4ecfe8d3fb56e73d194eb9ebdb4c7656e3
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-06-06 09:55:48 +0200

    Changed CKEDITOR.filter signature.

--- plugins/widget/plugin.js
+++ plugins/widget/plugin.js
@@ -2211,7 +2211,7 @@
 		var filter = editables[ editableName ];
 
 		if ( !filter ) {
-			filter = editableDefinition.allowedContent ? new CKEDITOR.filter( editableDefinition.allowedContent, this.editor ) : this.editor.filter.clone();
+			filter = editableDefinition.allowedContent ? new CKEDITOR.filter( editableDefinition.allowedContent ) : this.editor.filter.clone();
 
 			editables[ editableName ] = filter;
 
